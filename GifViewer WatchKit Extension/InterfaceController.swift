42487bb8cc48e2e86d32474ce100e82ed71758fe
Jason Peebles <jsp@Salo.local>
2015-04-02 08:26:25 -0700

    Iniital commit

---
+++ GifViewer WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  GifViewer WatchKit Extension
+//
+//  Created by Jason Peebles on 2015-04-02.
+//  Copyright (c) 2015 Themis Solutions Inc. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ GifViewerKit/GifViewerKit.h
@@ -0,0 +1,19 @@
+//
+//  GifViewerKit.h
+//  GifViewerKit
+//
+//  Created by Jason Peebles on 2015-04-02.
+//  Copyright (c) 2015 Themis Solutions Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GifViewerKit.
+FOUNDATION_EXPORT double GifViewerKitVersionNumber;
+
+//! Project version string for GifViewerKit.
+FOUNDATION_EXPORT const unsigned char GifViewerKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GifViewerKit/PublicHeader.h>
+
+
